6e1b490e43acae6b1b194fdcdb774981653ec625
Fhikar Zul <Fhikarzul18@gmail.com>
2022-09-01 10:15:21 +0800

    Initial Commit

---
+++ TestingSwiftUI/TestingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSwiftUIApp.swift
+//  TestingSwiftUI
+//
+//  Created by Lucy on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
